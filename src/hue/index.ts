5df5ed774a388b646eaa2006d552bd1f1f170a2e
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2022-07-28 19:42:14 +0200

    Add authentication with Hue Bridge

--- src/hue/index.ts
+++ src/hue/index.ts
@@ -1,37 +1,53 @@
 import { LocalStorage, showToast, Toast } from "@raycast/api";
-import { discovery } from "node-hue-api";
+import { discovery, v3 } from "node-hue-api";
 import Style = Toast.Style;
+import Api from "node-hue-api/lib/api/Api";
 
-export async function getBridgeIpAddress(): Promise<string | undefined> {
-  try {
-    const fromLocalStorage = await LocalStorage.getItem<string>("bridgeIpAddress");
-    if (fromLocalStorage) {
-      return fromLocalStorage;
-    }
+const appName = "raycast_hue_extension";
 
-    const discoveryResults = await discovery.nupnpSearch();
-    if (discoveryResults.length === 0) {
-      // noinspection ExceptionCaughtLocallyJS
-      throw new Error("No bridges found");
-    }
-
-    // Ignoring that you could have more than one Hue Bridge on a network as this is unlikely in 99.9% of users situations
-    const fromDiscovery = discoveryResults[0].ipaddress;
-    LocalStorage.setItem("bridgeIpAddress", fromDiscovery).then();
-
-    return discoveryResults[0].ipaddress;
-  } catch (error) {
-    console.error(error);
-
-    if (error instanceof Error) {
-      showToast(Style.Failure, error.name, error.message).then();
-    } else {
-      showToast(
-        Style.Failure,
-        "Could not find any Hue Bridges",
-        "Please check your network connection and make sure you have a Hue Bridge connected to your network."
-      ).then();
-    }
-    return undefined;
+export async function getHueBridgeIpAddress(): Promise<string> {
+  const fromLocalStorage = await LocalStorage.getItem<string>("bridgeIpAddress");
+  if (fromLocalStorage !== undefined) {
+    return fromLocalStorage;
   }
+
+  const discoveryResults = await discovery.nupnpSearch();
+  if (discoveryResults.length === 0) {
+    throw new Error("Could not find a Hue Bridge");
+  }
+
+  // Ignoring that you could have more than one Hue Bridge on a network as this is unlikely in 99.9% of users situations
+  const fromDiscovery = discoveryResults[0].ipaddress;
+  LocalStorage.setItem("bridgeIpAddress", fromDiscovery).then();
+
+  return discoveryResults[0].ipaddress;
+}
+
+export function getUnauthenticatedApi(bridgeIpAddress: string): Promise<Api> {
+  return v3.api.createLocal(bridgeIpAddress).connect();
+}
+
+export async function getHueBridgeUsername(): Promise<string> {
+  const bridgeIpAddress = await getHueBridgeIpAddress();
+
+  const localStorageUsername = await LocalStorage.getItem<string>("username");
+
+  if (localStorageUsername !== undefined) {
+    return localStorageUsername;
+  }
+
+  // Create an unauthenticated instance of the Hue API so that we can create a new user
+  const unauthenticatedApi = await getUnauthenticatedApi(bridgeIpAddress);
+  const createdUser = await unauthenticatedApi.users.createUser(appName, "");
+
+  if (createdUser === undefined) {
+    throw new Error("Could not create a new user");
+  }
+
+  LocalStorage.setItem("username", createdUser.username).then();
+  return createdUser.username;
+}
+
+export async function getAuthenticatedApi(bridgeIpAddress: string, username: string): Promise<Api | undefined> {
+  return await v3.api.createLocal(bridgeIpAddress).connect(username.username);
 }
