fb321b4d7870eef6c43fe75b73561ca5b612bfe0
Hans van Luttikhuizen-Ross <pindab0ter@me.com>
2022-07-28 18:50:05 +0200

    Cache Bridge IP address

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import { List } from "@raycast/api";
 import { useState } from "react";
 import { getBridgeIpAddress } from "./hue";
+import { useCachedState } from "@raycast/utils";
 
 export default function Command() {
-  const [bridgeIpAddress, setBridgeIpAddress] = useState<string | undefined>(undefined);
-  const [isLoading, setIsLoading] = useState(true);
+  const [bridgeIpAddress, setBridgeIpAddress] = useCachedState<string | undefined>("bridgeIpAddress", undefined);
+  const [isLoading, setIsLoading] = useState(bridgeIpAddress === undefined);
 
-  getBridgeIpAddress().then((bridgeIpAddress) => {
-    setIsLoading(false);
-    setBridgeIpAddress(bridgeIpAddress);
-  });
+  if (bridgeIpAddress === undefined) {
+    getBridgeIpAddress().then((bridgeIpAddress) => {
+      setIsLoading(false);
+      setBridgeIpAddress(bridgeIpAddress);
+    });
+  }
 
   return <List isLoading={isLoading}>{bridgeIpAddress && <List.Item title={bridgeIpAddress} />}</List>;
 }
